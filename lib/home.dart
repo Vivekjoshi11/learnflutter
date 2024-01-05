f1bd10758d7e064fcb79d73c7b162c948048c76e
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-01-05 20:49:55 +0530

    .

--- lib/home.dart
+++ lib/home.dart
@@ -13,13 +13,32 @@ class Home_page extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          title: const Text("Vivek"),
+          title: const Text("Vivek's Catalog App"),
         ),
-        body: Center(
-          child: Text("welcome to $days of flutter by $name aa"),
+        // body: Center(
+        //   child: Text("welcome to $days of flutter by $name aa"),
         
+        // ),
+        body: Container(
+          //from constraints the children will follow the instruction from that 
+         constraints: const BoxConstraints(
+          minHeight: 70,
+          maxHeight: 200,
+          minWidth: 70,
+          maxWidth: 200
+         ),
+         height: 400,
+         width: 400,
+         color: Colors.green,
+         // it will follow the size of container and container enforcde the child to be the size that is why it is not visible green color
+            child: Container(
+              height: 400,
+              width: 400,
+              color: Colors.red,
+            )
+          
         ),
         drawer: const MyDrawer(),
       );
-  }
+  } 
 }
